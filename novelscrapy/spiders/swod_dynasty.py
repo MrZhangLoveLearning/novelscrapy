0915e83878a6767f50128462360289898564fe87
mrzhang <2529450174@qq.com>
2015-12-05 11:17:27 +0800

    complete the filter

--- novelscrapy/spiders/swod_dynasty.py
+++ novelscrapy/spiders/swod_dynasty.py
@@ -9,10 +9,10 @@ from novelscrapy.items import NovelscrapyItem
 class SwodDynastySpider(CrawlSpider):
     name = 'swod_dynasty'
     allowed_domains = ['tieba.baidu.com']
-    start_urls = ['http://tieba.baidu.com/f?kw=%E6%8B%A9%E5%A4%A9%E8%AE%B0&ie=utf-8&tab=good']
+    start_urls = ['http://tieba.baidu.com/f?kw=%E5%89%91%E7%8E%8B%E6%9C%9D&ie=utf-8&tab=good']
 
     rules = (
-        Rule(LinkExtractor(allow=('http://tieba.baidu.com/f\?kw=%E6%8B%A9%E5%A4%A9%E8%AE%B0&ie=utf-8&tab=good&cid=&pn=[0-9]*',),)),
+        Rule(LinkExtractor(allow=('http://tieba.baidu.com/f\?kw=%E5%89%91%E7%8E%8B%E6%9C%9D&ie=utf-8&tab=good&cid=&pn=[0-9]*',),)),
         Rule(LinkExtractor(allow=r'/p/[0-9]*'), callback='parse_item', follow=True),
     )
 
@@ -26,4 +26,7 @@ class SwodDynastySpider(CrawlSpider):
         i['art_type'] = u"剑王朝"
         i['art_title'] = response.xpath('//title/text()').extract()[0]
         i['content'] = u''.join(response.xpath("//div[@class='p_content ']/cc/div").extract())
-        return i
+        if i['art_title'].find(u"卷") >= 0:
+            return i
+        else:
+            print 'this is not a good url：'+i['art_url']
